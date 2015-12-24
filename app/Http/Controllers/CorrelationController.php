8c0df81689dc2e23c93a4e2cd5cc9bdf6879b2b9
Daiyrbek Artelov <dartelov@intelliants.com>
2015-12-24 11:35:42 +0600

    CorrelationController added

---
+++ app/Http/Controllers/CorrelationController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class CorrelationController extends Controller
+{
+    /**
+     * Index page of application. Shows table, where user can put values to be calculated
+     */
+    public function index()
+    {
+        
+    }
+
+}
